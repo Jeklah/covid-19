93136aca6d19f5a0f7845c45c87f530c397f6965
Jeklah <arthurbowers@gmail.com>
2020-12-20 18:54:35 +0000

    Corrected mask count.

--- covid.py
+++ covid.py
@@ -6,19 +6,19 @@ peopleDictionary = []
 # simulation of a single person
 class Person():
     def __init__(self, startingImmunity):
-        self.maskChance = random.randint(1,100)
         if random.randint(0, 100) < startingImmunity:
             self.immunity = True
         else:
             self.immunity = False
 
         self.contagiousness = 0
-        self.mask = False
         self.contagiousDays = 0
+        self.maskChance = 0
+        self.maskCounted = False
+        self.mask = False
 
         # use gaussian distribution for number of friends, average is 5 friends.
         self.friends = int((norm.rvs(size=1, loc=0.5, scale=0.15)[0]*10).round(0))
-        self.maskCounted = False
 
     def wearMask(self):
         #print(self.maskChance)
@@ -49,8 +49,6 @@ def runDay(daysContagious, lockdown):
     # this section simulates the spread, so it only operates on contagious people, thus:
     for person in peopleDictionary:
         person.maskChance = random.randint(1, 100)
-
-
 
     for person in [person for person in peopleDictionary if person.contagiousness > 0 and person.friends > 0]:
         peopleCouldMeetToday = int(person.friends / 2)
@@ -93,6 +91,9 @@ for x in range(0, 100):
         if person.mask == True and person.maskCounted == False:
             person.maskCounted = True
             maskCount += 1
+        elif person.mask == False and person.maskCounted == True:
+            person.maskCounted = False
+            maskCount -= 1
 
     runDay(daysContagious, lockdown)
     write = str(len([person for person in peopleDictionary if person.contagiousness > 0])) + '\n'
